897312a49c23bb52cddf9ec1ac74327230560e96
Eric Farias <ericfarrias@hotmail.com>
2018-06-20 03:27:56 -0300

    Model e Repository adicionados para Cidade e Estado

--- src/main/java/com/mercado/loja/LojaApplication.java
+++ src/main/java/com/mercado/loja/LojaApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mercado.loja.model.Categoria;
+import com.mercado.loja.model.Cidade;
+import com.mercado.loja.model.Estado;
 import com.mercado.loja.model.Produto;
 import com.mercado.loja.repository.CategoriaRepository;
+import com.mercado.loja.repository.CidadeRepository;
+import com.mercado.loja.repository.EstadoRepository;
 import com.mercado.loja.repository.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class LojaApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository repoProd;
+	
+	@Autowired
+	private EstadoRepository estadoRepo;
+	
+	@Autowired
+	private CidadeRepository cidadeRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
@@ -48,6 +58,20 @@ public class LojaApplication implements CommandLineRunner{
 		repoCat.saveAll(Arrays.asList(cat1, cat2));
 		repoProd.saveAll(Arrays.asList(p1,p2,p3));
 		
+		
+		/*Cidade e Estado*/
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.setCidades(Arrays.asList(c1));
+		est2.setCidades(Arrays.asList(c2, c3));
+		
+		estadoRepo.saveAll(Arrays.asList(est1, est2));
+		cidadeRepo.saveAll(Arrays.asList(c1,c2,c3));		
 		
 	}
 }
