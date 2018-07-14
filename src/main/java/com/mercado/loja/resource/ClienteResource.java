71e0e8610c843c42e99b5778677b211c073214ff
Eric Farias <ericfarrias@hotmail.com>
2018-07-14 01:45:09 -0300

    POST Cliente

--- src/main/java/com/mercado/loja/resource/ClienteResource.java
+++ src/main/java/com/mercado/loja/resource/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.mercado.loja.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,13 +12,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mercado.loja.dto.ClienteDTO;
+import com.mercado.loja.dto.ClienteNewDTO;
 import com.mercado.loja.model.Cliente;
 import com.mercado.loja.service.ClienteService;
 
@@ -33,7 +37,12 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(service.find(id));
 	}
 	
-	//inserir
+	@PostMapping
+	public ResponseEntity<Void> inserir(@RequestBody ClienteNewDTO clienteNovo){
+		Cliente cliente = service.insert(service.fromDTO(clienteNovo));
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@PutMapping(value="/{id}")
 	public ResponseEntity<Void> atualizar(@Valid @RequestBody ClienteDTO cliente, @PathVariable Integer id){
--- src/main/java/com/mercado/loja/service/ClienteService.java
+++ src/main/java/com/mercado/loja/service/ClienteService.java
@@ -8,16 +8,25 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mercado.loja.dto.ClienteDTO;
+import com.mercado.loja.dto.ClienteNewDTO;
+import com.mercado.loja.model.Cidade;
 import com.mercado.loja.model.Cliente;
+import com.mercado.loja.model.Endereco;
+import com.mercado.loja.model.enums.TipoCliente;
 import com.mercado.loja.repository.ClienteRepository;
+import com.mercado.loja.repository.EnderecoRepository;
 import com.mercado.loja.service.exception.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> cliente = repo.findById(id);
@@ -26,6 +35,14 @@ public class ClienteService {
 				+ ", type: " + Cliente.class.getName()));
 	}
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		Cliente cliente = repo.save(obj);
+		enderecoRepository.saveAll(cliente.getEnderecos());
+		return cliente;
+	}
+	
 	public void update(Cliente cliente) {
 		find(cliente.getId());
 		repo.save(cliente);
@@ -47,6 +64,21 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO clienteNovo) {
 		Cliente clienteAtual = find(clienteNovo.getId());
 		return updateData(clienteAtual, clienteNovo);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipoCliente()));
+		Cidade cid = new Cidade(objDTO.getCidade(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cid, cli);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
 	}
 	
 	// Método de serviço que atualiza o cliente com as novas informações.
