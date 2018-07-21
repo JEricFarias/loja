818951f5ebf22c7b123dbf738eb1b23297d9140a
Eric Farias <ericfarrias@hotmail.com>
2018-07-21 01:46:08 -0300

    Validação personalizada: email unico na inserção de Cliente

--- src/main/java/com/mercado/loja/service/annotation/ClienteInsertValidator.java
+++ src/main/java/com/mercado/loja/service/annotation/ClienteInsertValidator.java
@@ -6,12 +6,18 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.mercado.loja.dto.ClienteNewDTO;
 import com.mercado.loja.model.enums.TipoCliente;
+import com.mercado.loja.repository.ClienteRepository;
 import com.mercado.loja.resource.exception.ValidationMessageError;
 import com.mercado.loja.service.annotation.util.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,7 +33,9 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new ValidationMessageError("cpfOuCnpj", "CNPJ inválido"));
 		}
-
+		if (clienteRepository.findByEmail(objDto.getEmail()) != null) {
+			list.add(new ValidationMessageError("email", "Email já cadastrado"));
+		}
 		for (ValidationMessageError e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
