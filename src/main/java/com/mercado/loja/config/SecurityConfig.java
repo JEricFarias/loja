81e43b6899f8d41634274c0a9145dc8fb88f426c
Eric Farias <ericfarrias@hotmail.com>
2018-08-13 21:53:50 -0300

    Implementando Autorização

--- src/main/java/com/mercado/loja/config/SecurityConfig.java
+++ src/main/java/com/mercado/loja/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mercado.loja.security.JWTAuthenticationFilter;
+import com.mercado.loja.security.JWTAuthorizationFilter;
 import com.mercado.loja.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), userDetailsService, jwtUtil));
 	}
 	
 	@Override
--- src/main/java/com/mercado/loja/security/JWTUtil.java
+++ src/main/java/com/mercado/loja/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -22,5 +23,30 @@ public class JWTUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + milis))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		
+		if(claims != null) {
+			String username = claims.getSubject();
+			Date date = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			return username != null &&  date != null && now.before(date);
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getUsername(String token) {
+		String user = getClaims(token).getSubject(); 
+		return  user;
 	}
 }
